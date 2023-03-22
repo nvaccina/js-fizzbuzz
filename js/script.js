eb2f046db4ba46fc1170a8f8f291781e2b109c53
nvaccina <niccolovac@gmail.com>
2023-03-22 18:40:02 +0100

    Applicazione stili agli elementi contenuti nel DOM

--- js/script.js
+++ js/script.js
@@ -9,12 +9,16 @@ for (let i = 1; i <= 100; i++) {
   //Condizioni numeri 3 e 5
   if (!(i % 3) && !(i % 5)){
     box.append('FizzBuzz');
+    box.classList.add('orange');
   }else if (!(i % 3)){
     box.append('Fizz');
+    box.classList.add('red');
   }else if (!(i % 5)){
     box.append('Buzz');
+    box.classList.add('yellow');
   }else{
     box.append(`${i}`); 
+    box.classList.add('number');
   } 
   
   console.log(box, i);
